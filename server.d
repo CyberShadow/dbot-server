9badc29f3eb4dce5083cfd5cc7d98bee5768a6bd
Vladimir Panteleev <git@thecybershadow.net>
2016-04-27 08:48:11 +0000

    Initial commit

---
+++ server.d
@@ -0,0 +1,13 @@
+import ae.net.asockets;
+import ae.sys.net.ae;
+
+void main()
+{
+	// TODO: Start web server
+
+	// TODO: Grab initial GitHub data (pulls to test)
+
+	// TODO: Start clients
+
+	socketManager.loop();
+}
